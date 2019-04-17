da33b65755078c51ff768205fcd2ef81d5131400
James Haley <james.haley@ucdenver.edu>
2019-04-17 11:55:03 -0600

    adding the run_cycles script

---
+++ run_cycles.m
@@ -0,0 +1,11 @@
+function output = run_cycles(num_cycles)
+% function output = run_cycles(num_cycles)
+% function runs the cycling routine
+% Inputs:
+%   num_cycles - Integer number of cycles to run
+% Output:
+%   output - integer indicating success or failure
+
+
+
+end
